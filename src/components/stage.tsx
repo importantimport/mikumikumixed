db86507bad665ded374c4a0766ccb742374d4e1b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-04 19:10:49 +0800

    refactor(stage): update controls

--- src/components/stage.tsx
+++ src/components/stage.tsx
@@ -1,21 +1,35 @@
 import { Environment, SoftShadows } from '@react-three/drei'
 import { IfInSessionMode } from '@react-three/xr'
-import { useControls } from 'leva'
+import { folder, useControls } from 'leva'
 
 export const Stage = ({ children }: React.PropsWithChildren) => {
   const {
-    background,
     backgroundBlurriness,
-    ground,
+    backgroundEnable,
+    backgroundIntensity,
+    backgroundRotation,
+    groundEnable,
+    groundHeight,
+    groundRadius,
+    groundScale,
     // preset,
   } = useControls('Stage', {
-    background: true,
-    backgroundBlurriness: {
-      max: 1,
-      min: 0,
-      value: 0.5,
-    },
-    ground: false,
+    background: folder({
+      backgroundBlurriness: {
+        max: 1,
+        min: 0,
+        value: 0.5,
+      },
+      backgroundEnable: true,
+      backgroundIntensity: 1,
+      backgroundRotation: [0, 0, 0],
+    }),
+    ground: folder({
+      groundEnable: false,
+      groundHeight: 39,
+      groundRadius: 100,
+      groundScale: 100,
+    }),
     // preset: 'dawn',
   })
 
@@ -23,10 +37,12 @@ export const Stage = ({ children }: React.PropsWithChildren) => {
     <>
       <IfInSessionMode deny="immersive-ar">
         <Environment
-          background={background}
+          background={backgroundEnable}
           backgroundBlurriness={backgroundBlurriness}
+          backgroundIntensity={backgroundIntensity}
+          backgroundRotation={backgroundRotation}
           files="https://cdn.jsdelivr.net/gh/pmndrs/drei-assets/hdri/kiara_1_dawn_1k.hdr"
-          ground={ground}
+          ground={groundEnable && { height: groundHeight, radius: groundRadius, scale: groundScale }}
         // preset={preset as 'dawn'}
         />
       </IfInSessionMode>
