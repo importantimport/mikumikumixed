f312efb88c12cf22ba2009797900d532ea35dd06
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-04 19:28:12 +0800

    refactor: adjust camera position

--- src/app.tsx
+++ src/app.tsx
@@ -20,14 +20,19 @@ export const App = () => {
     <>
       <button onClick={() => store.enterAR()} type="button">Enter AR</button>
       <button onClick={() => store.enterVR()} type="button">Enter VR</button>
-      <Canvas shadows style={{ flexGrow: 1, width: '100%' }}>
+      <Canvas camera={{ position: [0, 0, 2] }} shadows style={{ flexGrow: 1, width: '100%' }}>
         <XR store={store}>
-          <group position={[0, 0, 2.6]}>
+          <group position={[0, 0, 2]}>
             <XROrigin />
           </group>
           <Stage>
             <Model />
-            <OrbitControls makeDefault maxPolarAngle={Math.PI / 1.9} minPolarAngle={0} />
+            <OrbitControls
+              makeDefault
+              maxPolarAngle={Math.PI / 1.9}
+              minPolarAngle={0}
+              target={[0, 1, 0]}
+            />
           </Stage>
         </XR>
       </Canvas>
