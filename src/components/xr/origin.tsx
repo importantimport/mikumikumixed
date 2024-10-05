74e0fefa4e0e6cbcfbb8972391faa2f1edebed51
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-05 21:29:35 +0800

    refactor(origin/teleport-target): remove mesh

--- src/components/xr/origin.tsx
+++ src/components/xr/origin.tsx
@@ -9,20 +9,7 @@ export const Origin = () => {
   return (
     <>
       <XROrigin position={position} />
-      <TeleportTarget onTeleport={setPosition}>
-        <mesh position={[0, 0, 0]} scale={[0.5, 0.001, 0.5]}>
-          <sphereGeometry />
-          <meshPhysicalMaterial
-            color="white"
-            ior={1.5}
-            opacity={0.15}
-            specularColor="white"
-            specularIntensity={1}
-            transmission={1}
-            transparent
-          />
-        </mesh>
-      </TeleportTarget>
+      <TeleportTarget onTeleport={setPosition} />
     </>
   )
 }
